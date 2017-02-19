4eb1b36e6cf5930c642078eb0c24c550ac549424
Jeff Poyzner <holidaydoctor@hotmail.com>
2017-02-19 12:13:16 -0800

    more precise

--- cc.js
+++ cc.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 module.exports = {
 	arb: function() {
 		this.readings = 0;
+		this.flips = 0;
 		require('node-schedule').scheduleJob('*/1 * * *', function() {
+			this.readings++;
 			this.prices();
 		}.bind(this));
 	},
@@ -46,22 +48,49 @@ module.exports = {
 							// + " (" + parseInt(nowSeconds - Number(snap.LASTUPDATE)) + " SECONDS AGO)");
 					});
 
+					//JUST MAKE SURE LOWEST PRICE STAYS LOWEST FOR NOW
+					if (this.lowestSnap && lowestSnap.MARKET === this.lowestSnap.MARKET) {
+						this.currentFlipMins++;
+					} else {
+						if (this.lowestSnap) {
+							this.flips++;
+						}
+
+						if (this.flips) {
+							this.flipMinsAvg += (this.currentFlipMins - this.flipMinsAvg) / this.flips;
+						} else {
+							this.flipMinsAvg = 0;
+						}
+						
+						this.currentFlipMins = 0;
+					}
+
+					this.lowestSnap = lowestSnap;
+					this.highestSnap = highestSnap; ///
+
 					//console.log("LOWEST PRICE: $" + lowestSnap.PRICE + " (" + lowestSnap.MARKET + ")");
 					//console.log("HIGHEST PRICE: $" + highestSnap.PRICE + " (" + highestSnap.MARKET + ")");
 
 					var diff = (Number(highestSnap.PRICE) - Number(lowestSnap.PRICE));
 					var diffPercent = diff * 100 / Number(lowestSnap.PRICE);
 
-					if (this.readings++) {
+					if (this.readings > 1) {
 						this.avgarb += (diffPercent - this.avgarb) / this.readings;
+						this.rate += (diffPercent - this.diffPercent - this.rate) / this.readings - 1;
 					} else {
 						this.avgarb = diffPercent;
+						this.rate = 0;
 					}
+
+					this.diffPercent = diffPercent;
 
 					console.log(
 						diffPercent + "% ($"
 						+ diff + ", " + highestSnap.MARKET + "->" + lowestSnap.MARKET + ") "
 						+ now + ", AVG=" + this.avgarb + "%");
+
+					console.log("THIS FLIP: " + this.currentFlipMins + " mins (AVG:" + this.flipMinsAvg + " mins)");
+					console.log("AVG CHANGE PER MINUTE: " + this.rate + "%");
 				}
 			}.bind(this));
 		}.bind(this));
